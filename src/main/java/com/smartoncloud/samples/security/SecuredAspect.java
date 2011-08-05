d1537b70c82c63524bca292d2d9e4db343142e15
Milan Skuhra <milan.skuhra@gmail.com>
2011-08-05 22:54:33 +0200

    SecuredAspect is simpler by using annotation binding and pointcut designator @annotation

--- src/main/java/com/smartoncloud/samples/security/SecuredAspect.java
+++ src/main/java/com/smartoncloud/samples/security/SecuredAspect.java
@@ -1,12 +1,9 @@
 package com.smartoncloud.samples.security;
 
-import java.lang.reflect.Method;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import com.google.appengine.api.users.UserService;
@@ -15,21 +12,14 @@ import com.google.appengine.api.users.UserServiceFactory;
 @Aspect
 @Component
 public class SecuredAspect {
-    @Pointcut("execution(@Secured * *.*(..))")
+    @Pointcut("@annotation(annotation)")
     @SuppressWarnings("unused")
-    private void secured() {
+    private void secured(Secured annotation) {
     }
 
-    @Around("secured()")
-    public Object aroundCachedMethods(ProceedingJoinPoint thisJoinPoint)
+    @Around("secured(annotation)")
+    public Object aroundCachedMethods(ProceedingJoinPoint thisJoinPoint, Secured annotation)
 	    throws Throwable {
-	MethodSignature s = (MethodSignature) thisJoinPoint.getSignature();
-	Object target = thisJoinPoint.getTarget();
-	Class<? extends Object> clazz = target.getClass();
-	String methodName = s.getMethod().getName();
-	Class<?>[] paramTypes = s.getParameterTypes();
-	Method declaredMethod = clazz.getDeclaredMethod(methodName, paramTypes);
-	Secured annotation = declaredMethod.getAnnotation(Secured.class);
 	boolean admin = annotation.admin();
 	UserService userService = UserServiceFactory.getUserService();
 	boolean userLoggedIn = userService.isUserLoggedIn();
